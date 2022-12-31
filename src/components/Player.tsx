5cbb0b9ff9c44546cbd752739d451a2c6e743cd5
Chander Ramesh <chander@chanders-mbp-2.lan>
2022-12-30 17:32:26 -0800

    Color player walls the same as player icon

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,5 +1,6 @@
 import { Component, For } from 'solid-js'
 import { Player as PlayerModel } from '../models/player'
+import { PlayerUtils } from '../utils/player'
 
 interface PlayerProps {
   player: PlayerModel
@@ -11,7 +12,11 @@ const Player: Component<PlayerProps> = ({ player }: PlayerProps) => {
       {player.name}
       <div class="flex h-full gap-x-0.5 items-center justify-center">
         <For each={new Array(player.walls)}>
-          {() => <div class="w-1 h-4 bg-purple-400" />}
+          {() => (
+            <div
+              class={`w-1 h-4 ${PlayerUtils.getTailwindColor(player.color)}`}
+            />
+          )}
         </For>
       </div>
     </div>
