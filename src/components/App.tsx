e8810fe69fcbfc2bfd802538238311a38fbd3ec1
Chander Ramesh <chander@chanders-mbp-2.lan>
2023-01-01 15:09:04 -0800

    Remove unnecessary Monetag component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,11 @@
 import type { Component } from 'solid-js'
 import { Routes, Route } from '@solidjs/router'
 import Game from './Game'
-import Monetag from './Monetag'
 
 const App: Component = () => {
   return (
     <Routes>
       <Route path="/" element={<Game />} />
-      <Route path="/about" element={<Monetag />} />
     </Routes>
   )
 }
