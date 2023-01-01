7a8b6c6f81c455b55b76938f893899a219adb131
Chander Ramesh <chander@chanders-mbp-2.lan>
2023-01-01 14:31:03 -0800

    Add Monetag page to prove ownership

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,15 @@
 import type { Component } from 'solid-js'
+import { Routes, Route } from '@solidjs/router'
 import Game from './Game'
+import Monetag from './Monetag'
 
 const App: Component = () => {
-  return <Game />
+  return (
+    <Routes>
+      <Route path="/" element={<Game />} />
+      <Route path="/about" element={<Monetag />} />
+    </Routes>
+  )
 }
 
 export default App
---
+++ src/components/Monetag.tsx
@@ -0,0 +1,12 @@
+import type { Component } from 'solid-js'
+
+const Monetag: Component = () => {
+  return (
+    <div class="flex flex-col w-full h-screen justify-center items-center">
+      <h1 class="text-xl font-bold">Monetag</h1>
+      <p>Hello, Monetag!</p>
+    </div>
+  )
+}
+
+export default Monetag
